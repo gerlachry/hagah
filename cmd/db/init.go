61782a3a8e9df9375c5d1ec067b7e1742ef7c19c
Ryan Gerlach <gerlachry@gmail.com>
2017-11-24 21:32:13 -0500

    example full text search queries

--- cmd/db/init.go
+++ cmd/db/init.go
@@ -10,6 +10,26 @@ import (
 
 // Postgresql initialization script for scripture database.
 
+// post indexing step to add index for full text search
+// update scriptures set tsv = setweight(to_tsvector(text), 'A');
+
+// Query for full text searching, top 10 hits
+// select b.name, chapter, verse, text, ts_rank(tsv, q) as rank, q
+// from scriptures s, books b, plainto_tsquery('the word') q
+// where s.book_id = b.id and tsv @@ q
+// order by rank desc
+// limit 10;
+
+// Query for adding highlighting to full text search
+// select name as book, chapter, verse, ts_headline(text, q)
+// from (
+//  select b.name, chapter, verse, text, ts_rank(tsv, q) as rank, q
+// from scriptures s, books b, plainto_tsquery('god the father') q
+// where s.book_id = b.id and tsv @@ q
+// order by rank desc
+// limit 10) a
+// order by rank desc;
+
 func main() {
 	log.Println("Starting db init")
 	var schema = `
@@ -36,6 +56,7 @@ func main() {
 			chapter integer NOT NULL,
 			verse integer NOT NULL,
 			text text NOT NULL,
+			tsv tsvector,
 			CONSTRAINT scriptures_unq UNIQUE (book_id, chapter, verse)
 		);
 
